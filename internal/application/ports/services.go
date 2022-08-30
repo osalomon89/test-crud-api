0d217c2a2cf1c8f3abc3ede27bcf7b9afcdf2f41
osalomon89 <ext_ossalomo@mercadolibre.com>
2022-08-30 09:14:00 -0300

    kvs implementation

--- internal/application/ports/services.go
+++ internal/application/ports/services.go
@@ -6,6 +6,7 @@ import (
 	"github.com/osalomon89/test-crud-api/internal/domain"
 )
 
+//go:generate mockgen -source=./services.go -destination=../test/mocks/item_service_mock.go -package=mocks
 type ItemService interface {
 	CreateItem(ctx context.Context, item domain.Item) (*domain.Item, error)
 	GetItemByID(ctx context.Context, itemID uint) (*domain.Item, error)
--- internal/infrastructure/repositories/mysql/config.go
+++ internal/infrastructure/repositories/mysql/config.go
@@ -1,4 +1,4 @@
-package repositories
+package mysql
 
 import (
 	"fmt"
